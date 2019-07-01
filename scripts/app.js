787aa78d4fed54cbc37b64acc786d3a2b6812867
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-01 22:51:25 +0100

    Home page set as default when website is opened

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,36 @@
-var xhr = newHXMLHttpRequest();
+// var xhr = newHXMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+// xhr.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         document.getElementById("data").innerHTML = this.responseText;
+//     }
+// };
 
-xhr.open("GET", )
+// xhr.open("GET", )
 
 
 
 console.log(location.hash);
 
 window.addEventListener("hashchange", function() {
-    var contentDiv = $("#content");
-    contentDiv.innerHTML = location.hash;
+        var contentDiv = document.getElementById("content");
 
-})
+        //Set the "content" div innerHTML based on the fragment identifier.
+        contentDiv.innerHTML = location.hash;
+
+        //If no fragment identifier is provided,
+        if (!location.hash) {
+            location.hash = "#homepage";
+        }
+
+        //Navigate once to the initial hash value.
+        navigate();
+
+        //Navigate whenever the fragment identifier value changes.
+        window.addEventListener("hashchange", navigate);
+
+    }
+
+)
 
 console.log(location.hash);

67a9af8d583038d094dbeb98565790b455f9e40f
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-01 18:12:51 +0100

    Footer was updated with col classes and added links to Contact and FAQs

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,21 @@
-$('#helpModal').on('shown.bs.modal', function() {
-    $('#helpInput').trigger('focus')
+var xhr = newHXMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
+
+xhr.open("GET", )
+
+
+
+console.log(location.hash);
+
+window.addEventListener("hashchange", function() {
+    var contentDiv = $("#content");
+    contentDiv.innerHTML = location.hash;
+
 })
+
+console.log(location.hash);

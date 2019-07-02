20ade007213a25c99e8eb083386aefaf0d59d433
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-02 15:51:19 +0100

    Function for request and state confirmation

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
- // Create a new AJAX request for fetching the partial HTML file.
- var request = new XMLHttpRequest();
+  // Create a new AJAX request for fetching the partial HTML file.
+  var xhr = newHXMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("data").innerHTML = this.responseText;
+      }
+  };

30f96640364aa9b44e9f8cb9365b17b3b8af7678
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-02 16:30:02 +0100

    Function added to call the callback with the content loaded from the file.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
   // Create a new AJAX request for fetching the partial HTML file.
   var xhr = newHXMLHttpRequest();
 
+  // Load page when readyState is 4 and status is 200
   xhr.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           document.getElementById("data").innerHTML = this.responseText;
       }
+  };
+
+  // Call the callback with the content loaded from the file.
+  xhr.onload = function() {
+      callback(xhr.responseText);
   };

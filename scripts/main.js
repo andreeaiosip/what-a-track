40f35666f866e4f8cd26ac26345fe26d498574d5
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-03 10:30:13 +0100

    Call the callback with the content loaded from the file.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,30 @@
   // Invoke function expression, so no global variable are introduced.
 
-  // Load page when readyState is 4 and status is 200
-  xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          $("#data").innerHTML = this.responseText;
-      }
-  };
   (function() {
 
-  // Call the callback with the content loaded from the file.
-  xhr.onload = function() {
-      callback(xhr.responseText);
-  };
+          // Create a new AJAX request for fetching the partial HTML file.
+          var xhr = newHXMLHttpRequest();
+
+          // Load page when readyState is 4 and status is 200
+          xhr.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                  $("#data").innerHTML = this.responseText;
+              }
+          };
+
+          // Call the callback with the content loaded from the file.
+          xhr.onload = function() {
+              callback(xhr.responseText);
+          };
 
 
-  // Listen for the href value changes.
-  window.addEventListener("hashchange", function() {
+          // Listen for the href value changes.
+          window.addEventListener("hashchange", function() {
 
-      // Target the div with the id of "data"
-      var dataDiv = $("#data");
+              // Target the div with the id of "data".
+              var dataDiv = $("#data");
 
-      // "Data" div will have the current hash value.
-      dataDiv.innerHTML = location.hash;
-  });      };
+              // "Data" div will have the current hash value.
+              dataDiv.innerHTML = location.hash;
+          });
+      };

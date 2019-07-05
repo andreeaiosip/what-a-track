fb29d08b31f0328994f867b816ff2c2f6e8f58db
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-05 11:56:00 +0100

    Revert "Home page set as default when website is opened"
    This reverts commit dc9f446a949d0706e8f293b95eace1c1cec7f9ef.

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,21 @@
-// var xhr = newHXMLHttpRequest();
+var xhr = newHXMLHttpRequest();
 
-// xhr.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         document.getElementById("data").innerHTML = this.responseText;
-//     }
-// };
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
 
-// xhr.open("GET", )
+xhr.open("GET", )
 
 
 
 console.log(location.hash);
 
 window.addEventListener("hashchange", function() {
-        var contentDiv = document.getElementById("content");
+    var contentDiv = $("#content");
+    contentDiv.innerHTML = location.hash;
 
-        //Set the "content" div innerHTML based on the fragment identifier.
-        contentDiv.innerHTML = location.hash;
-
-        //If no fragment identifier is provided,
-        if (!location.hash) {
-            location.hash = "#homepage";
-        }
-
-        //Navigate once to the initial hash value.
-        navigate();
-
-        //Navigate whenever the fragment identifier value changes.
-        window.addEventListener("hashchange", navigate);
-
-    }
-
-)
+})
 
 console.log(location.hash);

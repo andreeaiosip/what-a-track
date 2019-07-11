8a8b96253ab424b69b5e99087627a674c60da166
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-11 14:14:53 +0100

    Function name changed from findMusic to getArtist

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
-// // // Musixmatch API details
 const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
-const apiURL = "http://api.musixmatch.com/ws/1.1/";
-let userSearched = $("#q").val();
+const apiURL = "http://api.musixmatch.com/ws/1.1/"
+let userSearched = $("#q").val()
+fetch(apiURL + "track.search?q=" + userSearched + "&apikey=" + apiKey)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        const results = data.message.body.track_list;
+        let idOfArtist = results[0].track.artist_id
+        getArtist(idOfArtist)
+    });
 
 
-fetch(apiURL + "track.search?q" + userSearched + "&apikey=" + apiKey)
+function getArtist(artistID) {
+    console.log(artistID);
+    fetch(apiURL + "artist.get?artist_id=" + artistID + "&apikey=" + apiKey)
+        .then(res => res.json())
+        .then(data => console.log(data));
+};

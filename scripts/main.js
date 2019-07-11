707dbec18aa1df5e269a505048b35aa8b12b1267
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-11 11:38:01 +0100

    Musixmatch API key and URL added for fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
 // // // Musixmatch API details
+const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
+const apiURL = "http://api.musixmatch.com/ws/1.1/";
+let userSearched = $("#q").val();
+
+
+fetch(apiURL + "track.search?q" + userSearched + "&apikey=" + apiKey)

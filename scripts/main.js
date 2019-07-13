0656c9bb68f5f3c495691e6be13b18895c3453b9
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-13 23:07:43 +0100

    Added missing semi-colons

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,21 @@
 const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
-const apiURL = "http://api.musixmatch.com/ws/1.1/"
-let userSearched = $("#q").val()
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        const results = data.message.body.track_list;
-        let idOfArtist = results[0].track.artist_id
-        getArtist(idOfArtist)
-    });
+const apiURL = "https://api.musixmatch.com/ws/1.1/";
+let userSearched = $("#q").val();
 
 function getData(userSearched) {
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const results = data.message.body.track_list;
+            let idOfArtist = results[0].track.artist_id;
+            getArtist(idOfArtist)
+        });
+}
 
 function getArtist(artistID) {
-    console.log(artistID);
+    console.log(artistID)
+
     fetch(apiURL + "artist.get?artist_id=" + artistID + "&apikey=" + apiKey)
         .then(res => res.json())
         .then(data => console.log(data));
-};
+}

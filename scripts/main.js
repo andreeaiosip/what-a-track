7f46a6f8881aee3d965f799b7f503396fa8f3fe7
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-13 15:57:18 +0100

    Function name for fetch added as getData

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
 const apiURL = "http://api.musixmatch.com/ws/1.1/"
 let userSearched = $("#q").val()
-fetch(apiURL + "track.search?q=" + userSearched + "&apikey=" + apiKey)
     .then(res => res.json())
     .then(data => {
         console.log(data);
@@ -10,6 +9,7 @@ fetch(apiURL + "track.search?q=" + userSearched + "&apikey=" + apiKey)
         getArtist(idOfArtist)
     });
 
+function getData(userSearched) {
 
 function getArtist(artistID) {
     console.log(artistID);

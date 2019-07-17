e57194a7f644ff9d4cdcc6666c315899a4cbc9bd
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-17 12:44:09 +0100

    Function to retrieve data changed to AJAX  because data is send back jsonp

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,35 @@
-const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
+// Set event Listener on Search Button
+const searchButton = document.querySelector('#user-search-button');
+const lyricsSearch = document.querySelector('#query-lyrics');
 
-// Musixmatch APIurl
+
+searchButton.addEventListener('click', (event) => {
+    const lyricsSearch = lyricsSearch.value;
+    getData(lyricsSearch);
+})
+
+
+const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
 const apiURL = "https://api.musixmatch.com/ws/1.1/";
 
-// Terms the user is inserting the search box
-let userSearched = $('#q').val();
+function getData(lyricsSearch) {
+    let results = document.getElementById("output").textContent;
+    console.log(results)
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: apiKey,
+            track_id: lyricsSearch,
+            format: "jsonp",
+            callback: "jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.search",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        contentType: 'application/json',
+        success: function(data) {
+            console.log(data)
 
-// Results printed in the HTML div section "output"
-let output = $('#output');
-
-output.innerHTML = "test test";
-
-
-function getData(userSearched) {
-    fetch(apiURL + "track.search?q=" + userSearched + "&apikey=" + apiKey, {
-            mode: 'no-cors'
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            const results = data.message.body.track_list;
-            let idOfArtist = results[0].track.artist_id;
-            getArtist(idOfArtist)
-        });
-}
-
-
-function getArtist(artistID) {
-    console.log(artistID)
-
-    fetch(apiURL + "artist.get?artist_id=" + artistID + "&apikey=" + apiKey)
-        .then(res => res.json())
-        .then(data => console.log(data));
+        }
+    });
 }

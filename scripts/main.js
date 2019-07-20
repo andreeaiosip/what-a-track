2e7a3ef6fa51c9de4c2992b711f6c84057c73cbb
Andrea Iosip <andrea.iosip@gmail.com>
2019-07-20 12:47:50 +0100

    Two radio buttons added for the option to pick from looking for lyrics by artist or by song - re-added after double checking the documentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,92 @@
 const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
 const apiURL = "https://api.musixmatch.com/ws/1.1/";
+const form = document.querySelector('#form');
 
 
-// Set event Listener on Search Button
-const searchButton = document.querySelector("#user-search-button");
 
 // Link the Search Button to the search field
 searchButton.addEventListener('click', () => {
     const lyricsSearch = ("#query-lyrics").value;
     getData(lyricsSearch);
 })
+//  form event listener
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    // 3 values - radio x 2, text input
+    let selected;
+
+    // selectors
+    const query = document.querySelector('#query-lyrics').value;
+    const radio = document.querySelectorAll('[type="radio"]');
+
+    // decide what was selected
+    radio.forEach(item => {
+        if (item.checked) selected = item.value;
+    })
+
+    getLyrics(selected, query);
+}
 
 
+function getLyrics(selected, query) {
 
-function getData() {
 
-    // Variable set for the search section field
-    const queryLyrics = document.querySelector("#query-lyrics").value;
+    if (selected == 'artist') {
+        data = {
+            apikey: apiKey,
+            q_artist: query,
+            f_has_lyrics: true,
+            format: "jsonp",
+            callback: "jsonp_callback"
+        }
+    } else {
+        data = {
+            apikey: apiKey,
+            q_track: query,
+            f_has_lyrics: true,
+            format: "jsonp",
+            callback: "jsonp_callback"
+        }
+    }
 
-    // Variable set for the output section
-    const outputSection = document.getElementById("#output");
-    document.querySelector("#output").textContent = "";
 
     $.ajax({
         type: "GET",
-        data: {
-            apikey: apiKey,
-            q_track: queryLyrics,
-            q_artist: queryLyrics,
-            format: "jsonp",
-            callback: "jsonp_callback"
-        },
+        data: data,
 
         // matcher.lyrics.get API method takes two parameters: q_track and q_artist
-        url: apiURL + "matcher.lyrics.get",
+        url: apiURL + 'track.search',
         dataType: "jsonp",
         jsonpCallback: "jsonp_callback",
         contentType: "application/json",
         success: function(data) {
-            let lyricsResults = data.message.body;
-            outputSection.innerHTML += `<thead>
-                                           <tr>
-                                            <th scope="col">Song</th>
-                                          <th scope="col">Artist</th>
-                                        </tr>
-                                     </thead>`;
-            lyricsResults.forEach(function(item) {
-                outputSection.innerHTML += `<tbody>
-                                            <tr>
-                                                <td>${item.tracklist.q_track}</td>
-                                                <td>${item.tracklist.q_artist}</td>
-                                            </tr>
-                                        </tbody>`;
-            })
+            let results = data.message.body;
+            console.log(data)
         }
     })
 }
+
+// function appendToPage(results) {
+
+//     const tbody = document.querySelector("tbody");
+//     tbody.innerHTML = "";
+//     console.log(results)
+
+//     results.track_list.forEach(item => {
+//         tbody.innerHTML +=
+//             `<tr>
+//             <td>${item.track.artist_name}</td>
+//             <td>${item.track.track_name}</td>
+//             <td class="getLyrics" data-trackid="${item.track.track_id}">Get Lyrics</td>
+//         </tr>`;
+//     });
+
+
+//     let getLyrics = document.querySelectorAll('.getLyrics');
+//     getLyrics.forEach(item => item.addEventListener('click', (event) => {
+//         const trackID = event.currentTarget.dataset.trackid;
+//         console.log(trackID);
+//         getLyrics(trackID)
+//     }))
+// }

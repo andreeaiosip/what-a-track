d6ad33b557fcbad69109bdfcf9c5a858f687bcfc
Andrea Iosip <andrea.iosip@gmail.com>
2019-08-18 13:03:36 +0100

    Function for bringing the top tracks from Ireland on the homepage

---
+++ assets/js/chart-tracks-get.js
@@ -0,0 +1,55 @@
+const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
+const apiURL = "https://api.musixmatch.com/ws/1.1/";
+
+
+// API method for the CHART lookup
+
+function getChartTracks(appendToPageChartResults) {
+    urlExt = 'chart.tracks.get';
+
+    $.ajax({
+        type: "GET",
+        data: {
+            apikey: apiKey,
+            page: 1, // results only on homepage
+            page_size: 6, // 6 songs returned
+            chart_name: "top", // top tracks in Ireland
+            country: "ie",
+            f_has_lyrics: 1,
+            format: "jsonp",
+            callback: "jsonp_callback"
+
+        },
+        url: apiURL + urlExt,
+        dataType: "jsonp",
+        jsonpCallback: "jsonp_callback",
+        contentType: "application/json",
+        success: function(results) {
+            let results = results.message.body.track_list;
+            appendToPageChartResults(results);
+            console.log(results);
+        }
+    })
+}
+
+function appendToPageChartResults(results) {
+
+    const container = document.querySelector("#top-tracks");
+    container.innerHTML = "";
+    console.log(results);
+
+    // Print results on the page
+    // results.forEach(item => {
+    //     container.innerHTML +=
+    //         <div class="container-fluid">
+    //             <div class="card bg-dark text-white col-sm-12 col-md-4">
+    //                 <img src="..." class="card-img" alt="..."> // wil be a general music related image (no album image is sent back in the response).
+    //                      <div class="card-img-overlay">
+    //                         <h5 class="card-title">{track_name}</h5> // track name
+    //                             <h6>{artist_name}</h6> //artist name
+
+    //                      </div> 
+    //             </div>
+
+    // })
+}

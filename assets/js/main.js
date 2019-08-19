47c8c1a254bb0a5296c355d970ea62e933750a4e
Andrea Iosip <andrea.iosip@gmail.com>
2019-08-19 21:15:07 +0100

    Code commented out to test a new function only

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,132 +53,131 @@ $(window).bind("load", function() {
 });
 
 
-// Add event listener to the search button to trigger the search for the value typed in the search box
-searchButton.addEventListener('click', () => {
-    const form = document.querySelector('#form');
-    const query = document.querySelector('#query-lyrics').value;
-    const radio = document.querySelectorAll('[type="radio"]');
+// // Add event listener to the search button to trigger the search for the value typed in the search box
+// searchButton.addEventListener('click', () => {
+//     const form = document.querySelector('#form');
+//     const query = document.querySelector('#query-lyrics').value;
+//     const radio = document.querySelectorAll('[type="radio"]');
 
-    let selected;
-    radio.forEach(item => {
-        if (item.checked) selected = item.id;
-    })
+//     let selected;
+//     radio.forEach(item => {
+//         if (item.checked) selected = item.id;
+//     })
 
-    getLyrics(query, selected);
-})
+//     getLyrics(query, selected);
+// })
 
-//Enter key pressed, triggers search button click event
-$('#query-lyrics').keypress(function(e) {
-    if (e.which == 13) {
-        $('#searchButton').click();
-    }
-});
+// //Enter key pressed, triggers search button click event
+// $('#query-lyrics').keypress(function(e) {
+//     if (e.which == 13) {
+//         $('#searchButton').click();
+//     }
+// });
 
-function getLyrics(query, selected) {
+// function getLyrics(query, selected) {
 
-    // If artist radio button option is selected
-    let data;
-    if (selected == 'artist') {
-        data = {
-            apikey: apiKey,
-            q_artist: query,
-            f_has_lyrics: 1,
-            page: 1, // results only on homepage
-            page_size: 100, // 100 artists returned
-            s_artist_rating: "desc",
-            format: "jsonp",
-            callback: "jsonp_callback"
-        }
+//     // If artist radio button option is selected
+//     let data;
+//     if (selected == 'artist') {
+//         data = {
+//             apikey: apiKey,
+//             q_artist: query,
+//             f_has_lyrics: 1,
+//             page: 1, // results only on homepage
+//             page_size: 100, // 100 artists returned
+//             s_artist_rating: "desc",
+//             format: "jsonp",
+//             callback: "jsonp_callback"
+//         }
 
-        // API method for the artist lookup
-        urlExt = 'artist.related.get'
-
-
-        // If song radio button option is selected
-    } else if (selected == 'song') {
-        data = {
-            apikey: apiKey,
-            q_track: query,
-            f_has_lyrics: 1,
-            page: 1, // results only on homepage
-            page_size: 100, // 100 artists returned
-            s_track_rating: "desc",
-            format: "jsonp",
-            callback: "jsonp_callback"
-        }
-
-        // API method for the song lookup
-        urlExt = 'track.search'
-    }
-
-    $.ajax({
-        type: "GET",
-        data: data,
-        url: apiURL + urlExt,
-        dataType: "jsonp",
-        jsonpCallback: "jsonp_callback",
-        contentType: "application/json",
-        success: function(results) {
-
-            if (selected == 'artist') {
-                results = results.message.body.artist_list;
-                appendToPageArtistResults(results);
-            } else {
-                results = results.message.body.track_list;
-                appendToPageSongResults(results);
-            }
-        }
-    })
-
-}
-
-function appendToPageSongResults(results) {
-
-    const container = document.querySelector("#container");
-    container.innerHTML = "";
-    console.log(results);
-
-    // Print results on the page as a table layout
-    results.forEach(item => {
-        container.innerHTML +=
-            `<div class="container-songs">
-            <div class="card bg-dark text-white col-sm-12 col-md-6 mx-auto">
-           
-
-           <h5>${item.track.track_name}</h5> 
-           <h6>${item.track.artist_name}</h6>
-            <!-- <h6 class="getLyricsNow" data-trackid="${item.track.track_id}">Get Lyrics</h6> -->
-        </div>
-        </div>`;
-    });
+//         // API method for the artist lookup
+//         urlExt = 'artist.search'
 
 
-    let getLyricsNow = document.querySelectorAll('.getLyricsNow');
-    getLyricsNow.forEach(item => item.addEventListener('click', (event) => {
-        const trackID = event.currentTarget.dataset.track_id;
-        console.log(trackID);
-        getLyrics(trackID)
-    }))
-}
+//         // If song radio button option is selected
+//     } else if (selected == 'song') {
+//         data = {
+//             apikey: apiKey,
+//             q_track: query,
+//             f_has_lyrics: 1,
+//             page: 1, // results only on homepage
+//             page_size: 100, // 100 artists returned
+//             s_track_rating: "desc",
+//             format: "jsonp",
+//             callback: "jsonp_callback"
+//         }
 
-function appendToPageArtistResults(results) {
-    const container = document.querySelector("#container");
-    container.innerHTML = "";
-    console.log(results);
+//         // API method for the song lookup
+//         urlExt = 'track.search'
+//     }
 
-    // Print results on the page 
-    results.forEach(item, index) => {
-            $('.row').append(`<div class="col-sm-12 col-md-6 col-lg-4">
-        <div class="card my-5"
-        <img scr="${artist.cover}" class="card-img-top" alt="artist album cover">
-        </div>
-        </div>
-        container.innerHTML +=
-            `<div class="container-artists">
-                      <div class="card bg-dark text-white col-sm-12 col-md-6 mx-auto">
-              <h5 class="card-title">${item.artist.artist_name}</h5> 
-                                     <h6>${item.artist.artist_country}</h6>
-                               </div> 
-                        </div>`
-    });
-}
+//     $.ajax({
+//         type: "GET",
+//         data: data,
+//         url: apiURL + urlExt,
+//         dataType: "jsonp",
+//         jsonpCallback: "jsonp_callback",
+//         contentType: "application/json",
+//         success: function(results) {
+
+//             if (selected == 'artist') {
+//                 results = results.message.body.artist_list;
+//                 appendToPageArtistResults(results);
+//             } else {
+//                 results = results.message.body.track_list;
+//                 appendToPageSongResults(results);
+//             }
+//         }
+//     })
+
+// }
+
+// // Print results for the SONG results
+// function appendToPageSongResults(results) {
+
+//     const container = document.querySelector("#container");
+//     container.innerHTML = "";
+//     console.log(results);
+
+//     // Print results for the SONG results as Bootstrap cards
+
+//     //////////// I want to include the lyrics snippet (getSnippet function created in a separate file) of the song on the right hand side /////
+
+//     results.forEach(item => {
+//         container.innerHTML +=
+//             `<div class="container-songs">
+//             <div class="card bg-dark text-white col-sm-12 col-md-6 mx-auto">
+
+
+//            <h5>${item.track.track_name}</h5> 
+//            <h6>${item.track.artist_name}</h6>
+//             <!-- <h6 class="getLyricsNow" data-trackid="${item.track.track_id}">Get Lyrics</h6> -->
+//         </div>
+//         </div>`;
+//     });
+
+
+//     let getLyricsNow = document.querySelectorAll('.getLyricsNow');
+//     getLyricsNow.forEach(item => item.addEventListener('click', (event) => {
+//         const trackID = event.currentTarget.dataset.trackID;
+//         console.log(trackID);
+//         getLyrics(trackID)
+//     }))
+// }
+
+
+// // Print results for the ARTIST search
+// function appendToPageArtistResults(results) {
+//     const container = document.querySelector("#container");
+//     container.innerHTML = "";
+//     console.log(results);
+//     results.forEach(item => {
+//         container.innerHTML +=
+//             `<div class="container-artists">
+//                       <div class="card bg-dark text-white col-sm-12 col-md-6 mx-auto">
+//               <h5 class="card-title">${item.artist.artist_name}</h5> 
+//                                      <h6>${item.artist.artist_country}</h6>
+//                                </div> 
+//                         </div>`
+//     });
+// }

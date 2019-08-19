ceb0b95382dda3e88d0175140d11e036457603ad
Andrea Iosip <andrea.iosip@gmail.com>
2019-08-19 22:17:34 +0100

    Revert "Branch test-charts merged with master"
    This reverts commit e0de9d0808faa52c7e977d1f59872fbd87e99d29.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,122 +1,183 @@
 const apiKey = "e422a6beda4a794b4e5e2a03ad47ae5c";
 const apiURL = "https://api.musixmatch.com/ws/1.1/";
 
-// Add event listener to the search button to trigger the search for the value typed in the search box
-searchButton.addEventListener('click', () => {
-    const form = document.querySelector('#form');
-    const query = document.querySelector('#query-lyrics').value;
-    const radio = document.querySelectorAll('[type="radio"]');
 
-    let selected;
-    radio.forEach(item => {
-        if (item.checked) selected = item.id;
-    })
+// When page loads, print top 6 tracks from Ireland
+$(window).bind("load", function() {
 
-    getLyrics(query, selected);
-})
+    function getChartTracks(appendToPageChartResults) {
+        urlExt = 'chart.tracks.get';
+
+        $.ajax({
+            type: "GET",
+            data: {
+                apikey: apiKey,
+                page: 1, // results only on homepage
+                page_size: 6, // 6 songs returned
+                chart_name: top, // top tracks in Ireland
+                country: ie,
+                f_has_lyrics: 1,
+                format: "jsonp",
+                callback: "jsonp_callback"
+
+            },
+            url: apiURL + urlExt,
+            dataType: "jsonp",
+            jsonpCallback: "jsonp_callback",
+            contentType: "application/json",
+            success: function(results) {
+                let results = results.message.body.track_list;
+                appendToPageChartResults(results);
+                console.log(results);
+            }
+        })
+    }
 
 
-$('#query-lyrics').keypress(function(e) {
-    if (e.which == 13) { //Enter key pressed
-        $('#searchButton').click(); //Trigger search button click event
+    function appendToPageChartResults(results) {
+
+        const container = document.querySelector("#container");
+        container.innerHTML = "";
+        console.log(results);
+        results.forEach(item => {
+            container.innerHTML +=
+                `<div class="container">
+                <div class="card bg-dark text-white col-sm-12 col-md-4">
+                            <h5 class="card-title">${item.track.track_name}</h5>
+                                <h6>${item.track.artist_name}</h6> 
+                         </div> 
+                </div>
+   `
+        })
     }
 });
 
-function getLyrics(query, selected) {
 
-    // If artist radio button option is selected
-    let data;
-    if (selected == 'artist') {
-        data = {
-            apikey: apiKey,
-            q_artist: query,
-            f_has_lyrics: true,
-            page_size: 100,
-            s_artist_rating: "ASC",
-            format: "jsonp",
-            callback: "jsonp_callback"
-        }
+// // Add event listener to the search button to trigger the search for the value typed in the search box
+// searchButton.addEventListener('click', () => {
+//     const form = document.querySelector('#form');
+//     const query = document.querySelector('#query-lyrics').value;
+//     const radio = document.querySelectorAll('[type="radio"]');
 
-        // API method for the artist lookup
-        urlExt = 'artist.search'
+//     let selected;
+//     radio.forEach(item => {
+//         if (item.checked) selected = item.id;
+//     })
 
+//     getLyrics(query, selected);
+// })
 
-        // If song radio button option is selected
-    } else if (selected == 'song') {
-        data = {
-            apikey: apiKey,
-            q_track: query,
-            f_has_lyrics: true,
-            s_track_rating: true,
-            g_commontrack: true,
-            page_size: 100,
-            s_track_rating: "ASC",
-            format: "jsonp",
-            callback: "jsonp_callback"
-        }
+// //Enter key pressed, triggers search button click event
+// $('#query-lyrics').keypress(function(e) {
+//     if (e.which == 13) {
+//         $('#searchButton').click();
+//     }
+// });
 
-        // API method for the song lookup
-        urlExt = 'track.search'
-    }
+// function getLyrics(query, selected) {
 
-    $.ajax({
-        type: "GET",
-        data: data,
-        url: apiURL + urlExt,
-        dataType: "jsonp",
-        jsonpCallback: "jsonp_callback",
-        contentType: "application/json",
-        success: function(results) {
+//     // If artist radio button option is selected
+//     let data;
+//     if (selected == 'artist') {
+//         data = {
+//             apikey: apiKey,
+//             q_artist: query,
+//             f_has_lyrics: 1,
+//             page: 1, // results only on homepage
+//             page_size: 100, // 100 artists returned
+//             s_artist_rating: "desc",
+//             format: "jsonp",
+//             callback: "jsonp_callback"
+//         }
 
-            if (selected == 'artist') {
-                results = results.message.body.artist_list;
-                appendToPageArtistResults(results);
-            } else {
-                results = results.message.body.track_list;
-                appendToPageSongResults(results);
-            }
-        }
-    })
-
-}
-
-function appendToPageSongResults(results) {
-
-    const tbody = document.querySelector("#tbody");
-    tbody.innerHTML = "";
-    console.log(results);
-
-    // Print results on the page as a table layout
-    results.forEach(item => {
-        tbody.innerHTML +=
-            `<tr>
-            <td>${item.track.artist_name}</td>
-            <td>${item.track.track_name}</td>
-            <td class="getLyricsNow" data-trackid="${item.track.track_id}">Get Lyrics</td>
-        </tr>`;
-    });
+//         // API method for the artist lookup
+//         urlExt = 'artist.search'
 
 
-    let getLyricsNow = document.querySelectorAll('.getLyricsNow');
-    getLyricsNow.forEach(item => item.addEventListener('click', (event) => {
-        const trackID = event.currentTarget.dataset.trackid;
-        console.log(trackID);
-        getLyrics(trackID)
-    }))
-}
+//         // If song radio button option is selected
+//     } else if (selected == 'song') {
+//         data = {
+//             apikey: apiKey,
+//             q_track: query,
+//             f_has_lyrics: 1,
+//             page: 1, // results only on homepage
+//             page_size: 100, // 100 artists returned
+//             s_track_rating: "desc",
+//             format: "jsonp",
+//             callback: "jsonp_callback"
+//         }
 
-function appendToPageArtistResults(results) {
-    const tbody = document.querySelector("#tbody");
-    tbody.innerHTML = "";
-    console.log(results);
+//         // API method for the song lookup
+//         urlExt = 'track.search'
+//     }
 
-    // Print results on the page as a table layout
-    results.forEach(item => {
-        tbody.innerHTML +=
-            `<tr>
-                <td>${item.artist.artist_name}</td>
-                <td>${item.artist.artist_country}</td>
-                <td class="getLyricsNow" data-trackid="${item.artist.artist_id}">Find Lyrics</td>
-            </tr>`;
-    });
-}
+//     $.ajax({
+//         type: "GET",
+//         data: data,
+//         url: apiURL + urlExt,
+//         dataType: "jsonp",
+//         jsonpCallback: "jsonp_callback",
+//         contentType: "application/json",
+//         success: function(results) {
+
+//             if (selected == 'artist') {
+//                 results = results.message.body.artist_list;
+//                 appendToPageArtistResults(results);
+//             } else {
+//                 results = results.message.body.track_list;
+//                 appendToPageSongResults(results);
+//             }
+//         }
+//     })
+
+// }
+
+// // Print results for the SONG results
+// function appendToPageSongResults(results) {
+
+//     const container = document.querySelector("#container");
+//     container.innerHTML = "";
+//     console.log(results);
+
+//     // Print results for the SONG results as Bootstrap cards
+
+//     //////////// I want to include the lyrics snippet (getSnippet function created in a separate file) of the song on the right hand side /////
+
+//     results.forEach(item => {
+//         container.innerHTML +=
+//             `<div class="container-songs">
+//             <div class="card bg-dark text-white col-sm-12 col-md-6 mx-auto">
+
+
+//            <h5>${item.track.track_name}</h5> 
+//            <h6>${item.track.artist_name}</h6>
+//             <!-- <h6 class="getLyricsNow" data-trackid="${item.track.track_id}">Get Lyrics</h6> -->
+//         </div>
+//         </div>`;
+//     });
+
+
+//     let getLyricsNow = document.querySelectorAll('.getLyricsNow');
+//     getLyricsNow.forEach(item => item.addEventListener('click', (event) => {
+//         const trackID = event.currentTarget.dataset.trackID;
+//         console.log(trackID);
+//         getLyrics(trackID)
+//     }))
+// }
+
+
+// // Print results for the ARTIST search
+// function appendToPageArtistResults(results) {
+//     const container = document.querySelector("#container");
+//     container.innerHTML = "";
+//     console.log(results);
+//     results.forEach(item => {
+//         container.innerHTML +=
+//             `<div class="container-artists">
+//                       <div class="card bg-dark text-white col-sm-12 col-md-6 mx-auto">
+//               <h5 class="card-title">${item.artist.artist_name}</h5> 
+//                                      <h6>${item.artist.artist_country}</h6>
+//                                </div> 
+//                         </div>`
+//     });
+// }
